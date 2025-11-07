67c65b9e91e4bbbe95f357efa623ccf9df150fdc
shahidOmer <shahidomer06@gmail.com>
2025-11-07 21:48:41 +0530

    added pinging route for backend to keep it live

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 // const API_BASE_URL = 'http://localhost:5000/api';
-const API_BASE_URL = ['https://nexora-c1zz.onrender.com/api'];
+const API_BASE_URL = window.location.hostname === 'localhost' 
+  ? 'http://localhost:5000/api'
+  : 'https://nexora-c1zz.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
