1ab88dfcbdd496e07d7ac316d7d75ece0815491b
shahidOmer <shahidomer06@gmail.com>
2025-11-07 21:26:33 +0530

    added pinging route for backend to keep it live

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ dotenv.config();
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  origin: ['http://localhost:3000',`https://nexora-client-7wjt.onrender.com`],
+  credentials: true
+}));
 const PORT = 5000
 
 
